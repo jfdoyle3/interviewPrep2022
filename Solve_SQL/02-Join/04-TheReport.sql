b9b17e69b0e581967ae4e9f6f1e5ad0b2fcefb12
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-10-12 18:46:07 -0400

    Completed The Report Query

--- Solve_SQL/02-Join/04-TheReport.sql
+++ Solve_SQL/02-Join/04-TheReport.sql
@@ -1,9 +1,10 @@
-SELECT students.name,  grades.grade, students.marks
+SELECT IF(grades.grade<8,"NULL",students.name),
+		  grades.grade,
+          students.marks
 FROM students
 JOIN grades ON (
                 students.marks >= grades.min_mark
                 AND
                 students.marks <= grades.max_mark
                 )
-
-ORDER BY grades.grade DESC, students.name DESC
+ORDER BY grades.grade DESC, students.name ASC
