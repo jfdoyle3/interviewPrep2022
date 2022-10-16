2acb0d4aab165cbbd001d03e7c4cd67cf31a724c
Jim Doyle <indellibleink@gmail.com>
2022-10-15 20:02:14 -0400

    Working on Mar Exploration

---
+++ Java-ThreeMonthPreparation/Week_2_Interview_Questions/08-MarsExploration.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'marsExploration' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+    // Stack & Queue
+    public static int marsExploration(String s) {
+
+       int errorCount=0;
+       String[] message=s.split("(?<=\\G.{3})");
+        System.out.println(Arrays.deepToString(message));
+       for(int idx=0; idx<message.length; idx++){
+           if(!message[idx].equals("SOS"))
+               errorCount++;
+       }
+        return errorCount;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.marsExploration(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
