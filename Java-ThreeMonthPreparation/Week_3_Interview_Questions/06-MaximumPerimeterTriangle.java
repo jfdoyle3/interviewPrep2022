937df55859c7f768c8bc58310ccc706e62018727
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-10-19 23:24:31 -0400

    Working on Maximum Perimeter Triangle

---
+++ Java-ThreeMonthPreparation/Week_3_Interview_Questions/06-MaximumPerimeterTriangle.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'maximumPerimeterTriangle' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY sticks as parameter.
+     */
+
+    public static List<Integer> maximumPerimeterTriangle(List<Integer> sticks) {      List<Integer> results=new ArrayList<>();
+        if(sticks.size()<3){
+          results.add(-1);
+          return results;
+        }
+          
+        // sides: A B C
+        // to see if triangle is true
+        // A+B>C, A+C>B, B+C>A
+        
+        results.add(33333);
+        return results;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> sticks = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.maximumPerimeterTriangle(sticks);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
