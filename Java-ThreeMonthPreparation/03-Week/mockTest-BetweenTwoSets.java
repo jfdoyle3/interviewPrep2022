06512278e965851375fefc061f877ae7e551f340
jfdoyle3 <36973560+jfdoyle3@users.noreply.github.com>
2022-11-03 18:59:14 -0400

    Working on mockTest-BetweenTwoSets

--- Java-ThreeMonthPreparation/03-Week/mockTest-BetweenTwoSets.java
+++ Java-ThreeMonthPreparation/03-Week/mockTest-BetweenTwoSets.java
@@ -1,59 +1,81 @@
-/*
- * Sandbox.java
- *
- * Copyright 2022 Jim Doyle <>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- *
- *
- */
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
+class Result {
 
-public class Sandbox {
+    /*
+     * Complete the 'getTotalX' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
 
-    public static void main (String[] args) {
-		List<Integer> listA=new ArrayList<>(Arrays.asList(2,4));
-		List<Integer> listB=new ArrayList<>(Arrays.asList(16,24,96));
+    public static int getTotalX(List<Integer> a, List<Integer> b){
+        
+        List<Integer> factorsList=new ArrayList<>();
+        Collections.sort(a);
+        Collections.sort(b);
+        
+        int maxNumListA=Collections.max(a);
+        int minNumListB=Collections.min(b);
 
-		Collections.sort(listA);
-		Collections.sort(listB);
-
-		int maxNumListA=Collections.max(listA);
-		int minNumListB=Collections.min(listB);
-
-		List<Integer> factorList=new ArrayList<>();
-		List<Integer> results=new ArrayList<>();
-
-		for (int i = maxNumListA; i <= minNumListB; ++i) {
-          if(i%maxNumListA==0)
-			factorList.add(i);
-		}
-
-		for (int factor : factorList){
-			for(int number : listB){
-				if(number%factor==0)
-					results.add(factor);
-
-		 }
-		}
-
-		System.out.println(results);
+        for(int i=maxNumListA; i<=minNumListB; i++){
+            if(i%maxNumListA==0)
+                factorsList.add(i);
+        }
+       // System.out.println(factorsList);
+       
+       for(int factor=0; factor<factorsList.size(); factor++){
+           
+           for(int number : b){
+               if(number%factorsList.get(factor)!=0){
+                  // System.out.println(factor);
+                   factorsList.remove(factor);
+                    
+               }
+           }
+       }
+     //  System.out.println(factorsList);
+       return factorsList.size(); 
     }
-//  Methods Start here
+}
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> brr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int total = Result.getTotalX(arr, brr);
+
+        bufferedWriter.write(String.valueOf(total));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
